7a3030cb9f10a81481b4080b93a6655036eba89d
Esra Zengin <esra.zengin@helloprint.com>
2023-05-27 23:43:45 +0200

    implemented initial settings stack

---
+++ app/types/initialSettingsStack.d.ts
@@ -0,0 +1,10 @@
+type RootStackParamList = {
+  InitialSettingsScreen: NavigatorScreenParams<MainStackParamList>;
+};
+
+type InitialSettingsStackParamList = {
+  WelcomeScreen: undefined;
+  GenderScreen: undefined;
+  AgeScreen: undefined;
+  LanguageScreen: undefined;
+};
