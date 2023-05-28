f825bff8d5e3428fbe10ff5ddcab502df36f3539
htc-pnr <htc.guder@gmail.com>
2023-05-28 16:23:59 +0200

    Remove realm_pass env variable

--- __tests__/EnvVar-test.ts
+++ __tests__/EnvVar-test.ts
@@ -1,12 +1,12 @@
 import 'react-native';
-import {REALM_PASS} from '@env';
+import {API_KEY} from '@env';
 
 describe('App', () => {
   it('gets env variable', (): void => {
     // Mock the environment variables
     jest.mock('react-native-dotenv', () => ({
-      REALM_PASS: 'test_secret_value',
+      API_KEY: 'test_secret_value',
     }));
-    expect(REALM_PASS).toBeDefined();
+    expect(API_KEY).toBeDefined();
   });
 });
