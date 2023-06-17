919e19095a5457424b4dd9e5b05c5c2c297c4e9f
Esra Zengin <esra.zengin@helloprint.com>
2023-06-18 01:32:00 +0200

    created seperate context for synced and local realms

--- app/context/SymplatorRealm/SymplatorRealmContext.tsx
+++
@@ -1,9 +0,0 @@
-import {createRealmContext} from '@realm/react';
-// import {BodyPart} from './BodyPart';
-// import {BodyPartTranslation} from './BodyPartTranslation';
-import {Symptom} from '../../models/Symptom';
-import {SymptomTranslation} from '../../models/SymptomTranslation';
-
-export const SymplatorRealmContext = createRealmContext({
-  schema: [Symptom, SymptomTranslation],
-});
