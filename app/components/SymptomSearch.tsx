b79089f6f0628eb0a4783b2d96fe138ff4898757
htc-pnr <htc.guder@gmail.com>
2023-07-09 13:23:56 +0200

    fix: search query

--- app/components/SymptomSearch.tsx
+++ app/components/SymptomSearch.tsx
@@ -1,40 +1,50 @@
-import {Symptom} from '../models/Symptom';
-import React, {useState} from 'react';
+import {SyncedRealmContext} from '../context/SymplatorRealm/SyncedRealmContext';
+import {SymptomSchema} from '../models/Symptom';
+import React, {useState, useMemo, useEffect} from 'react';
 import {View, FlatList, Text} from 'react-native';
 import {TextInput} from 'react-native-paper';
-import Realm from 'realm';
+
+const {useQuery} = SyncedRealmContext;
 
 const SymptomSearch = () => {
   const [searchQuery, setSearchQuery] = useState('');
   const [results, setResults] = useState([]);
+  const objects = useQuery(SymptomSchema);
 
   const handleSearch = async (searchText: string) => {
     setSearchQuery(searchText);
 
     try {
-      const realm = await Realm.open({
-        schema: [Symptom],
-      });
-
-      const objects = realm.objects('Symptom');
       // Split the search query into separate words
       const searchWords = searchQuery.split(' ');
 
       // Filter the objects based on each word in the search query for each column
       const filteredResults = Array.from(
         objects
+          // .filtered(
+          //   searchWords
+          //     .map(word => `(translations.name CONTAINS[c] "${word}") `)
+          //     .join(' AND '),
+          // )
+          // .slice(),
           .filtered(
-            searchWords
-              .map(
-                word =>
-                  `(name CONTAINS[c] "${word}") OR (tag CONTAINS[c] "${word}") OR (detail CONTAINS[c] "${word}")`,
-              )
-              .join(' AND '),
+            'translations.language=$0 and ' +
+              '(translations.name CONTAINS[c] $1 or translations.detail CONTAINS[c] $1 or ' +
+              ' translations.tags CONTAINS[c] $1)',
+            'tr',
+            searchWords[0],
           )
-          .slice(),
       );
 
       setResults(filteredResults);
+      console.log('Filtered results:' + filteredResults.length);
+      // filteredResults.forEach(tr => {
+      //   console.log('item *********************** ');
+      //   console.log('name:' + tr.translations[0].name);
+      //   console.log('detail:' + tr.translations[0].detail);
+      //   console.log('tags:' + tr.translations[0].tags);
+      // });
+      console.log('----------------------------------');
     } catch (error) {
       console.error('Error querying Realm:', error);
     }
@@ -44,13 +54,16 @@ const SymptomSearch = () => {
     <View>
       <TextInput
         placeholder="Search symptom"
-        onChangeText={text => handleSearch(text)}
-        // onSubmitEditing={handleSearch}
+        onChangeText={text => {
+          if (text.length >= 3) {
+            handleSearch(text);
+          }
+        }}
       />
       <FlatList
         data={results}
         keyExtractor={(item, _) => item._id}
-        renderItem={({item}) => <Text>item.translations[0].name</Text>}
+        renderItem={({item}) => <Text>{item.translations[0].name}</Text>}
       />
     </View>
   );
