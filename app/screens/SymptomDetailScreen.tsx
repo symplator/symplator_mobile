39420b69838686dfc2cca0656e21016d67a2dbcb
htc-pnr <htc.guder@gmail.com>
2023-08-26 23:36:12 +0200

    feat: add show translated button

--- app/screens/SymptomDetailScreen.tsx
+++ app/screens/SymptomDetailScreen.tsx
@@ -1,26 +1,38 @@
 import {RouteProp} from '@react-navigation/native';
 import {StackNavigationProp} from '@react-navigation/stack';
-import {View, Text} from 'react-native';
+import {View, StyleSheet} from 'react-native';
 import React from 'react';
 import {TranslatedSymptomList} from '../components/TranslatedSymptomList';
+import {Button, Card, Text} from 'react-native-paper';
 type Props = {
   navigation: StackNavigationProp<RootStackParams, 'SymptomDetailScreen'>;
   route: RouteProp<RootStackParams, 'SymptomDetailScreen'>;
 };
 
 export const SymptomDetailScreen: React.FC<Props> = ({route}) => {
-
   const savedSymptom = route.params?.data;
-
-  console.log(savedSymptom.symptoms);
+  const [showTranslated, setShowTranslated] = React.useState<boolean>(false);
   return (
     <View>
-      <Text>{savedSymptom.tag}</Text>
-      <Text>{new Date(savedSymptom.date).toLocaleDateString()}</Text>
+      <Text variant="titleLarge" style={styles.tagText}>
+        {savedSymptom.tag} 
+      </Text>
+      <Text variant="titleLarge" style={styles.tagText}>
+        {new Date(savedSymptom.date).toLocaleDateString()}
+      </Text>
       <TranslatedSymptomList
-        isTranslated={false}
-        data={savedSymptom.symptoms}
+        isTranslated={showTranslated}
+        data={savedSymptom}
       />
+      <Button onPress={() => setShowTranslated(!showTranslated)}>
+        {showTranslated ? 'Show Original' : 'Show Translated'}
+      </Button>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  tagText: {
+    marginVertical: 10,
+  },
+});
